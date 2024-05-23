e04be5002851c3cb7a93a187fd45d09f4622da8f
ArcticLoon <travistaylor032@gmail.com>
2024-05-23 12:06:54 -0500

    Add files via upload

---
+++ album-name-formatter-Kyle.py
@@ -0,0 +1,334 @@
+# Sample plaintext input
+input_text = """
+Bear Creek ; Brandi Carlile
+Red ; Taylor Swift
+Folklore ; Taylor Swift
+Reputation ; Taylor Swift
+21 ; Adele
+Move Along ; All-American Rejects
+Oh, What A Life ; American Authors
+Dangerous Woman ; Ariana Grande
+thank u, next ; Ariana Grande
+Stories ; Avicii
+Let Go ; Avril Levine
+Millenium ; Backstreet Boys
+Black & Blue ; Backstreet Boys
+Duets ; Barbra Streisand
+Doom Days ; Bastille
+Abbey Road ; Beatles
+White album ; Beatles
+Friendly Fire ; Ben Abraham
+Sing to Me Instead ; Ben Platt
+Brand New ; Ben Rector
+Fireworks & Rollerblades ; Benson Boone
+Tchaikovsky & Dvorak: String Serenades ; Berlin Philharmonic & Herbert von Karajan
+I Am... Sasha Fierce ; Beyonce
+Cowboy Carter ; Beyonce
+Happier Than Ever ; Billie Eilish
+When We All Fall Asleep, Where Do We Go ; Billie Eilish
+By the Way, I Forgive You ; Brandi Carlile
+The Color Purple ; Revival Cast
+Doo-Wops & Hooligans ; Bruno Mars
+Only Human ; Calum Scott
+Requiem ; John Rutter
+Prom ; Original Broadway Cast
+Falling Into You ; Celine Dion
+The Rise and Fall of a Midwest Princess ; Chappell Roan
+Nine Track Mind ; Charlie Puth
+Fly ; The Chicks
+Home ; The Chicks
+Stripped ; Christina Aguilera
+Pictures of Mountains ; Cody Fry
+Viva la vida ; Coldplay
+X & Y ; Coldplay
+Kinky Boots ; Original Broadway Cast
+Postcards in the Sky ; David Archuleta
+Amor mi fa cantare ; Stefano Donaudy; Robert Guarino
+Intertwined ; Dodie
+You ; Dodie
+x ; Ed Sheeran
+Bright Star ; Original Broadway Cast
+Jerome Kern Songbook ; Ella Fitzgerald
+Cote D'Azur Concerts on Verve ; Ella Fitzgerald & Duke Ellington
+Ella and Louis Again ; Ella Fitzgerald & Louis Armstrong
+Goodbye Yellow Brick Road ; Elton John
+Our Version of Events ; Emeli Sande
+The Marshall Mathers LP2 ; Eminem
+Fallen ; Evanescence
+From Under the Cork Tree ; Fall Out Boy
+Infinity on High ; Fall Out Boy
+The Dutchess ; Fergie
+All the Feels ; Fitz and the Tantrums
+Fitz & the Tantrums ; Fitz and the Tantrums
+Fleet Foxes ; Fleet Foxes
+Lungs ; Florence + the Machine
+One by One ; Foo Fighters
+channel ORANGE ; Frank Ocean
+The Fray ; The Fray
+How to Save a Life ; The Fray
+Some Nights ; Fun.
+We are Young ; Fun.
+Sweeter ; Gavin Degraw
+Good Morning Revival ; Good Charlotte
+Is There Anybody Out There ; A Great Big World
+Fine Line ; Harry Styles
+Harry's House ; Harry Styles
+Whatever ; Hot Chelle Rae
+Hozier ; Hozier
+Wastleland, Baby! ; Hozier
+Night Visions ; Imagine Dragons
+Stranger Songs ; Ingrid Michaelson
+Choas and the Calm ; James Bay
+Flight ; James Francies
+Jason Derulo ; Jason Derulo
+We Sing. We Dance. We Steal Things. ; Jason Mraz
+Who Hurt You? ; Jensen McRae
+brent ii ; Jeremy Zucker & Celsey Culter
+I Cry When I Laugh ; Jess Glyne
+Georgia Pond ; Johnnyswim
+The Human Condition ; Jon Bellion
+Blue ; Joni Mitchell
+Dark in the City ; Jordy Searcy
+Human Now ; Joshua Speers
+Escape ; Journey
+Pep Talks ; Judah & the Lion
+Complete Decca Masters ; Judy Garland
+Cheers ; Jukebox the Ghost
+Jukebox the Ghost ; Jukebox the Ghost
+Let Live & Let Ghosts ; Jukebox the Ghost
+Inner Monologue ; Juilia Michaels
+The 20/20 Experience ; Justin Timberlake
+Golden Hour ; Kacey Musgraves
+Pagaent Material ; Kacey Musgraves
+Same Trailer Different Park ; Kacey Musgraves
+My Beautiful Dark Twisted Fantasy ; Kanye West
+Teenage Dream ; Katy Perry
+Animal ; Ke$ha
+Somewhere Only We Know ; Keane
+kelsea ; Kelsea Bellerini
+Black Panther The Album ; Kendrick Lamar
+Damn. ; Kendrick Lamar
+Celebrate Me Home ; Kenny Loggins
+American Teen ; Khalid
+Free Spirit ; Khalid
+Fun Machine ; Lake Street Dive
+The Last Five Years ; Original Broadway Cast
+~How I'm Feeling~ ; Lauv
+I met you when I was 18. ; Lauv
+Breakfast ; Lawrence
+It't Not All About You ; Lawrence
+23 ; Lawrence
+Stormy Weather ; Lena Horne
+Chichester Psalms & Facsimile ; Leonard Bernstein
+West Side Story ; Original Broadway Cast
+Peter Grimes ; Leonard Bernstein
+Montero ; Lil Nas X
+Cuz I Love You ; Lizzo
+Special ; Lizzo
+Pure Heroine ; Lorde
+Lukas Graham ; Lukas Graham
+Cleopatra ; Lumineers
+III ; Lumineers
+Circles ; Mac Miller
+Divine Feminine ; Mac Miller
+The Heist ; Macklemore & Ryan Lewis
+Heard It In A Past Life ; Maggie Rogers
+What the World Needs Now ; Mahalia Jackson
+Planet Her ; Doja Cat
+Pink Friday ; Nicki Minaj
+Songs About Jane ; Maroon 5
+Overexposed ; Maroon 5
+Thank You ; Megan Trainor
+Title ; Megan Trainor
+At the Crescendo ; Mel Torme
+It's Time ; Michael Buble
+Thriller ; Michael Jackson
+One Thing At A Time ; Morgan Wallen
+Babel ; Mumford & Sons
+The Black Parade ; My Chemical Romance
+Unforgettable ; Nat King Cole
+Picture Show ; Neon Trees
+Perception ; NF
+The End of Everything ; Noah Cyrus
+The Hardest Part ; Noah Cyrus
+Busyhead ; Noah Kahan
+Stick Season ; Noah Kahan
+No Strings Attached ; NSYNC
+GUTS ; Olivia Rodrigo
+Native ; OneRepublic
+Stampede: Vol. 1 ; Orville Peck
+The Truth About Love ; P!nk
+Death of a Bachelor ; Panic! At the Disco
+Pray For the Wicked ; Panic! At the Disco
+A Fever You Can't Sweat Out ; Panic! At the Disco
+Pretty. Odd. ; Panic! At the Disco
+Riot! ; Paramore
+Stranger in the Alps ; Phoebe Bridgers
+Life in Colour ; Picture This
+Planet Pit ; Pitbull
+Stoney ; Post Malone
+Hollywood's Bleeding ; Post Malone
+A Night at the Opera ; Queen
+A Letter to My Younger Self ; Quinn XCII 
+Wildfire ; Rachel Platten
+Rachel ; Rachel Willis-Sorensen
+Feels Like Today ; Rascal Flatts
+Me and My Gang ; Rascal Flatts
+Rationale ; Rationale
+Stadium Arcadium ; Red Hot Chili Peppers
+Don't You Fake It ; Red Jumpsuit Apparatus
+Far ; Regina Spector
+Everything to Everyone ; Renee Rapp
+Rent ; Original Broadway Cast
+Loud ; Rhianna
+Unapologetic ; Rhianna
+Phoenix ; Rita Ora
+Sound of Music ; Original Broadway Cast
+Do You Miss New York? ; Rosemary Clooney
+Singular Act I ; Sabrina Carpenter
+Montevallo ; Sam Hunt
+In the Lonely Hour ; Sam Smith
+The Thrill of It All ; Sam Smith
+Linger A While ; Samara Joy
+The Blessed Unrest ; Sara Bareilles
+Kaleidoscope Heart ; Sara Bareilles
+Little Voice ; Sara Bareilles
+Once Upon Another Time ; Sara Bareilles
+Waitress ; Sara Bareilles
+Freedom Child ; The Script
+#3 ; The Script
+A Twist In My Story ; Secondhand Serenade
+Handwritten ; Shawn Mendes
+Shawn Mendes ; Shawn Mendes
+This is Acting ; Sia
+Still Not Getting Any... ; Simple Plan
+SIX: Live On Opening Night ; Original Broadway Cast
+Company ; Original Broadway Cast
+The Shapes I'm In ; Stephen Day
+Guess I'm Grown Now ; Stephen Day
+Undergrad Romance and the Moses in Me ; Stephen Day
+Angel Face ; Stephen Sanchez
+Easy On My Eyes ; Stephen Sanchez
+Wicked ; Original Broadway Cast
+Rockstarr ; Taio Cruz
+Fearless (Taylor's Version) ; Taylor Swift
+One X ; Three Days Grace
+Three Days Grace ; Three Days Grace
+Hiding Place ; Tori Kelly
+Unbreakable Smile ; Tori Kelly
+Tracy Chapman ; Tracy Chapman
+California 37 ; Train
+Blue Neighbourhood ; Troye Sivan
+Blurryface ; twenty one pilots
+Us ; Us the Duo
+Father of the Bride ; Vampire Weekend
+The Addams Family ; Original Broadway Cast
+Chess in Concert ; Various artists
+Legally Blonde ; Various artists
+Songs for a New World ; Various artists
+La Traviata ; Vienna Philharmonic Orchestra
+Talking is Hard ; Walk the Moon
+Walk the Moon ; Walk the Moon
+We the Kings ; We the Kings
+After Hours ; The Weekend
+Beauty Beyond the Madness ; The Weekend
+Whitney ; Whitney Houston
+VHS ; X Ambassadors
+Orion ; X Ambassadors
+Welcome Home ; Zac Brown Band
+Mind of Mine ; Zayn
+Streets of Gold ; 3OH!3
+Want ; 3OH!3
+Call Me Irresponsible ; Michael Buble
+Crazy Love ; Michael Buble
+Ocean Avenue ; Yellowcard
+Enema of the State ; blink-182
+No Plans, No Helmets... Just Balls ; Simple Plan
+American Idiot ; Green Day
+The Best Damn Thing ; Avril Levine
+Astroworld ; Travis Scott
+Hotel California ; Eagles
+Confessions ; Usher
+Flower Boy ; Tyler, the Creator
+The Fame Monster ; Lady Gaga
+Like a Prayer ; Madonna
+SOS ; SZA
+Straight Outta Compton ; NWA
+A Love Supreme ; John Coltrane
+Take Care ; Drake
+I Never Loved A Man the Way I Loved You ; Aretha Franklin
+Carole King ; Tapestry
+Fleetwood Mac ; Rumors
+Back to Black ; Amy Winehouse
+Songs in the Key of Life ; Stevie Wonder
+In the Zone ; Britney Spears
+Breakaway ; Kelly Clarkson
+Fallen ; Evanescnce
+Good Girl Gone Bad ; Rhianna
+Hot Fuss ; Killers
+The Fame ; Lady Gaga
+Get Rich or Die Tryin' ; 50 Cent
+Camila ; Camila Cabello
+Northing but the Beat ; David Guetta
+Bad Blood ; Bastille
+Hands All Over ; Maroon 5
+Shania Twain ; Shania Twain
+Diamond Rio ; Diamond Rio
+Mountain Music ; Alabama
+Super Trouper ; ABBA
+Voulez-Vous ; ABBA
+Arrival ; ABBA
+New Gold Hits ; Four Seasons
+Diana Ross Presents the Jackson 5 ; Jackson 5
+Bad Girls ; Donna Summer
+Pendulum ; Creedence Clearwater Revival
+The Stranger ; Billy Joel
+Heart Like a Wheel ; Linda Ronstadt
+Presenting the Fabulous Ronnettes ; The Ronettes
+Where Did Our Love Go ; The Supremes
+Born in the USA ; Bruce Springsteen
+Diamond Star Halos ; Def Leppard
+Face Value ; Phil Collins
+Slippery When Wet ; Bon Jovi
+Keep the Fire ; Kenny Loggins
+Toto IV ; Toto
+One By One ; Foo Fighters
+Only By the Night ; Kings of Leon
+Infinity on High ; Fall Out Boy
+Meteora ; Linklin Park
+The Dark Side of the Moon ; Pink Floyd
+Paganini: 24 Caprices ; Itzhak Perlman
+Paganini: 24 Caprices for Solo Violin, Op. 1  ; Hilary Hahn
+Ain't That Good News ; Sam Cooke
+Good News ; Megan Thee Stallion
+Jolene ; Dolly Parton
+In the Heat of the Night ; Pat Benatar
+Glass Houses ; Billy Joel
+Christopher Cross ; Christopher Cross
+"""
+
+# Function to format the text into the desired output
+def format_text(input_text):
+    items = input_text.strip().split("\n")
+    formatted_items = []
+
+    for item in items:
+        title, artist = item.strip().split(" ; ")
+        name = f"{title} - {artist}"
+        image_name = f"{title}.png"
+        formatted_item = f"{{id: '{title}', name: '{name}', image: '{image_name}'}},"
+        formatted_items.append(formatted_item)
+
+    # Join the formatted items into a single string with newlines
+    formatted_output = "\n".join(formatted_items)
+
+    return formatted_output
+
+# Generate the formatted text
+formatted_text = format_text(input_text)
+
+# Write the formatted text to a file
+with open("formatted_output.txt", "w") as file:
+    file.write(formatted_text)
+
+print("Formatted text document created successfully.")
